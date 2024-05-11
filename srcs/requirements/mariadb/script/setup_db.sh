1cc819624493c16879ed5b2feab846f5c28d05bb
JeyDot-dev <72892603+JeyDot-dev@users.noreply.github.com>
2024-05-11 18:31:31 +0000

    fix to mariadb script, added more secrets

--- srcs/requirements/mariadb/script/setup_db.sh
+++ srcs/requirements/mariadb/script/setup_db.sh
@@ -9,9 +9,9 @@ chown -R mysql:mysql /var/lib/mysql /run/mysqld
 if [ ! -e "/var/lib/mysql/wordpress" ]
 then
     mariadb-install-db --datadir=/var/lib/mysql --user=mysql
-    mariadb -e "CREATE DATABASE \"$(cat /run/secrets/db_name)\" CHARACTER SET $CHAR_SET COLLATE $COLLATION;"
-    mariadb -e "CREATE USER \"$(cat /run/secrets/db_user)\"@'%' IDENTIFIED BY \"$(cat /run/secrets/db_password)\";"
-    mariadb -e "GRANT ALL PRIVILEGES ON \"$(cat /run/secrets/db_name).*\" TO \"$(cat /run/secrets/db_user)\"@'%';"
+    mariadb -e "CREATE DATABASE $(cat /run/secrets/db_name) CHARACTER SET $CHAR_SET COLLATE $COLLATION;"
+    mariadb -e "CREATE USER $(cat /run/secrets/db_user)@'%' IDENTIFIED BY $(cat /run/secrets/db_password);"
+    mariadb -e "GRANT ALL PRIVILEGES ON $(cat /run/secrets/db_name).* TO $(cat /run/secrets/db_user)@'%';"
     mariadb -e "FLUSH PRIVILEGES;"
 else
     echo "Database already present"
