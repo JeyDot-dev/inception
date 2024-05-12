cbc04c8cef9f70d62de149244755133e4772a182
JeyDot-dev <72892603+JeyDot-dev@users.noreply.github.com>
2024-05-12 00:33:29 +0000

    It's done, all the mandatory part seems to work, need testing at school

--- srcs/requirements/mariadb/script/setup_db.sh
+++ srcs/requirements/mariadb/script/setup_db.sh
@@ -6,13 +6,17 @@ set -x
 mkdir -p /var/lib/mysql /run/mysqld
 chown -R mysql:mysql /var/lib/mysql /run/mysqld
 
-if [ ! -e "/var/lib/mysql/wordpress" ]
+if [ ! -e "/var/lib/mysql/$(cat /run/secrets/db_name)" ]
 then
     mariadb-install-db --datadir=/var/lib/mysql --user=mysql
+    mysqld --user=mysql --datadir=/var/lib/mysql &
+    sleep 4
     mariadb -e "CREATE DATABASE $(cat /run/secrets/db_name) CHARACTER SET $CHAR_SET COLLATE $COLLATION;"
-    mariadb -e "CREATE USER $(cat /run/secrets/db_user)@'%' IDENTIFIED BY $(cat /run/secrets/db_password);"
-    mariadb -e "GRANT ALL PRIVILEGES ON $(cat /run/secrets/db_name).* TO $(cat /run/secrets/db_user)@'%';"
+    mariadb -e "CREATE USER '$(cat /run/secrets/db_user)'@'%' IDENTIFIED BY '$(cat /run/secrets/db_password)';"
+    mariadb -e "GRANT ALL PRIVILEGES ON $(cat /run/secrets/db_name).* TO '$(cat /run/secrets/db_user)'@'%';"
     mariadb -e "FLUSH PRIVILEGES;"
+    mysqladmin shutdown
+
 else
     echo "Database already present"
 fi
--- srcs/requirements/wordpress/script/set_wp.sh
+++ srcs/requirements/wordpress/script/set_wp.sh
@@ -1,3 +1,37 @@
-!#/bin/bash
+#!/bin/bash
+set -x
+if  wp core is-installed --path=/www ;then
+    echo "wordpress already configured"
+else
+    sleep 10
+    rm -rf www/*
+    wp core download --path=www
+    cp www/wp-config-sample.php www/wp-config.php
+    sed -i "s/database_name_here/$(cat /run/secrets/db_name)/" www/wp-config.php
+    sed -i "s/username_here/$(cat /run/secrets/db_user)/" www/wp-config.php
+    sed -i "s/password_here/$(cat /run/secrets/db_password)/" www/wp-config.php
+    sed -i "s/localhost/mariadb/" www/wp-config.php
+    sed -i "s/utf8/$CHAR_SET/" www/wp-config.php
+    sed -i "s/COLLATE', ''/COLLATE', '$COLLATION'/" www/wp-config.php
+    wp core install \
+        --path=www \
+        --url=https://$SERVER_NAME/ \
+        --title=$SITE_TITLE \
+        --admin_user=$(cat /run/secrets/wp_admin_name) \
+        --admin_password=$(cat /run/secrets/wp_admin_password) \
+        --admin_email=$(cat /run/secrets/wp_admin_email) \
+        --skip-email
+    wp user create $(cat /run/secrets/wp_user_name) \
+        --path=www \
+        $(cat /run/secrets/wp_user_email) \
+        --user_pass=$(cat /run/secrets/wp_user_password) \
+        --role=author
+    wp theme install typeflow --path=www --activate
+    chown -R phpfpm:phpfpm www
+    if ! wp core is-installed --path=www ;then
+        echo "error during installation"
+        exit 1
+    fi
+fi
 
-
+exec "$@"
